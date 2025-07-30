7a1f9c090e6136b972c4b781482d6775ddc5062d
psjy33 <112685292+psjy33@users.noreply.github.com>
2025-07-30 17:46:37 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 460 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+    int N, K;
+    scanf("%d %d", &N, &K);
+
+    int people[5001] = { 0 };
+    int result[5001];
+    int index = 0;
+    int count = 0;
+    int step = 0;
+
+    while (count < N) {
+        if (people[index] == 0) {
+            step++;
+            if (step == K) {
+                people[index] = 1;
+                result[count++] = index + 1;
+                step = 0;
+            }
+        }
+        index++;
+        if (index == N) index = 0;
+    }
+
+    printf("<");
+    for (int i = 0; i < N; i++) {
+        printf("%d", result[i]);
+        if (i != N - 1) printf(", ");
+    }
+    printf(">\n");
+
+    return 0;
+}
